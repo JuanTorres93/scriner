a6ea76d49feb8dbd2e2e11e466095330be78c731
Juan Torres <juan93git@gmail.com>
2025-08-06 13:23:15 +0200

    Function to remove mark by id

--- src/features/script/ScriptActions.jsx
+++ src/features/script/ScriptActions.jsx
@@ -1,4 +1,4 @@
-import { Editor } from "slate";
+import { Editor, Transforms, Text } from "slate";
 
 export const ScriptActions = {
   isMarkActive(editor, type) {
@@ -14,5 +14,18 @@ export const ScriptActions = {
 
   removeMark(editor, type) {
     Editor.removeMark(editor, type);
+  },
+
+  removeEditMarkByEditId(editor, type, targetEditId) {
+    for (const [node, path] of Editor.nodes(editor, {
+      at: [], // Whole document
+      match: Text.isText,
+    })) {
+      // Check if the node has the type and editId we are looking for
+      if (node[type] && node[type].editId === targetEditId) {
+        // Remove the 'type' property (e.g. 'sfx') only from that node
+        Transforms.unsetNodes(editor, type, { at: path });
+      }
+    }
   },
 };
