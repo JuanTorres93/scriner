490b6c1caf614128d73617c523dfbd2b40fb98b9
Juan Torres <juan93git@gmail.com>
2025-09-13 18:26:00 +0200

    First iteration of styling Problem section

--- src/presentation/features/landing/Problem.jsx
+++ src/presentation/features/landing/Problem.jsx
@@ -1,11 +1,32 @@
+import styled from "styled-components";
+import Section from "./Section";
+
+const StyledSection = styled(Section)`
+  img {
+    max-width: 45rem;
+    height: auto;
+    margin-bottom: 2rem;
+  }
+
+  em {
+    display: inline;
+    font-style: italic;
+    font-weight: var(--font-weight-medium);
+    color: var(--color-accent);
+  }
+`;
+
 function Problem() {
   return (
-    <section id="problem">
-      Crees que te falta creatividad, pero tienes un deseo ardiente de hacer
-      mejores vídeos. Tu proceso actual se basa en ver tu vídeo una y otra vez
-      en tu <em>timeline</em> esperando a que te llegue la iluminación para
-      saber qué añadir.
-    </section>
+    <StyledSection id="problem">
+      <img src="problem.png" alt="Un problema de creatividad" />
+      <p>
+        Crees que te falta creatividad, pero tienes un deseo ardiente de hacer
+        mejores vídeos. Tu proceso actual se basa en ver tu vídeo una y otra vez
+        en tu <em>timeline</em> esperando a que te llegue la iluminación para
+        saber qué añadir.
+      </p>
+    </StyledSection>
   );
 }
 
---
+++ src/presentation/features/landing/Section.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  display: flex;
+  gap: 8rem;
+  padding: 8rem 2rem;
+  max-width: 200rem;
+  margin: 0 auto;
+  font-size: var(--font-size-b2);
+  color: var(--color-grey-s2);
+  line-height: 1.4;
+`;
+
+export default StyledSection;
