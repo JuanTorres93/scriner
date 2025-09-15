6ed831e0035dfd37fa73254321013ebb03a4d25b
Juan Torres <juan93git@gmail.com>
2025-09-15 08:55:55 +0200

    Refactor Supabase client configuration to use environment variables.

--- src/infrastructure/supabase/client.js
+++ src/infrastructure/supabase/client.js
@@ -1,10 +1,8 @@
 // src/infrastructure/supabase/client.js
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://jcemvkcdgjapayhiuywk.supabase.co";
-// IMPORTANT: According to the Supabase documentation, there's no problem with exposing the public key as long as RLS are properly set up
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZW12a2NkZ2phcGF5aGl1eXdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3MTY2OTIsImV4cCI6MjA2OTI5MjY5Mn0.izdwLPI5t3bLlCNKa6LRkk6-5wlE_BnoysEN6MODOSg";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
